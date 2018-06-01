7a6ebe6765eb72e727022989181bb79efe4636d4
vlrkalinina <kalinina.valeri@gmail.com>
2018-06-02 01:20:00 +0700

    component Modify

--- src/components/Modify/index.js
+++ src/components/Modify/index.js
@@ -1,28 +1,111 @@
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+} from 'reactstrap';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { typesPriorities } from '../../helpers/const';
 
-const { object } = PropTypes;
+import './index.css';
 
-class List extends Component {
+const {object} = PropTypes;
+
+class Modify extends Component {
   static propTypes = {
     location: object,
   };
 
+  state = {
+    title: '',
+    description: '',
+    priority: 0,
+  };
+
+  componentWillMount() {
+    const {
+      props: {
+        items,
+        location: {
+          pathname,
+        },
+      },
+    } = this;
+
+    if (pathname !== '/modify/new') {
+      const idTask = pathname.substr(13);
+      const task = items.find(item => item.id === +idTask);
+
+      this.setState({
+        id: task.id,
+        title: task.title,
+        description: task.description,
+        priority: task.priority,
+        deadline: task.deadline,
+      });
+    }
+  }
+
   render() {
+    const {
+      state: {
+        title,
+        completed,
+        description,
+        priority,
+      },
+      props: {
+        location: {
+          pathname,
+        },
+      },
+    } = this;
+
     return (
-      <div>
-        qwe
-      </div>
+      <Form className="form">
+        <h2 className="form__title">{pathname === '/modify/new' && 'Новая задача'}</h2>
+        <FormGroup className="form__group">
+          <Label className="form__label">Название:</Label>
+          <Input className="form__input" value={title}/>
+        </FormGroup>
+        <FormGroup className="form__group">
+          <Label className="form__label">Описание:</Label>
+          <Input className="form__input" type="textarea" name="text" value={description}/>
+        </FormGroup>
+        <FormGroup className="form__group">
+          <Label className="form__label">Важность:</Label>
+          <Input className="form__input" type="select" name="select" id="exampleSelect" value={priority}>
+            {typesPriorities.map(type => <option key={type.id} value={type.id}>{type.name}</option>)}
+          </Input>
+        </FormGroup>
+        <FormGroup className="form__group">
+          <Label className="form__label">Дата выполнения:</Label>
+          <Input className="form__input from__date" type="date" name="date" id="exampleDate"
+                 placeholder="date placeholder"/>
+          <Input className="form__input from__date" type="time" name="time" id="exampleTime"
+                 placeholder="time placeholder"/>
+        </FormGroup>
+        <FormGroup check row>
+          <Button className="form__btn" color="primary">
+            {pathname === '/modify/new' ? 'Создать' : 'Редактировать'}
+          </Button>
+          <Button className="form__btn">Закрыть без сохранения</Button>
+        </FormGroup>
+      </Form>
     );
   }
 }
 
-const mapState = state => ({});
+const mapState = state => ({
+  items: state.items,
+});
 const mapDispatch = ({
-                       items: { add }
+                       items: {add},
                      }) => ({
   addItem: () => add(),
 });
 
-export default connect(mapState, mapDispatch)(List);
+export default connect(mapState, mapDispatch)(Modify);
