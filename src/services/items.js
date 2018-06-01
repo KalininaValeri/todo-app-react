81dd0217cc2a7ed61300078a538309ad6fa9fc9d
vlrkalinina <kalinina.valeri@gmail.com>
2018-06-01 22:00:51 +0700

    completed && delete

--- src/services/items.js
+++ src/services/items.js
@@ -1,22 +1,21 @@
 import { setLocalStorage, getDataLocalStorage } from '../helpers/handlerForLocalStorage';
+import { dispatch } from '@rematch/core';
 
 const arrayItems = [
   {
     id: 123,
     title: 'title 1',
     completed: false,
-    description: 'qwe, qwe, qwe, qwe',
+    description: 'Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.',
     priority: 0,
-    deadline: 'May 31 2018',
-    close_date: 'May 31 2018',
+    deadline: '1525798800000',
   }, {
     id: 563,
     title: 'title 2',
-    completed: false,
-    description: 'qwe, qwe, qwe, qwe',
-    priority: 0,
-    deadline: 'May 31 2018',
-    close_date: 'May 31 2018',
+    description: 'Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.',
+    priority: 2,
+    deadline: '1525798800000',
+    completed: '1525798800000',
   }
 ];
 
@@ -29,10 +28,37 @@ export const items = {
       setLocalStorage();
       return state;
     },
+    removeItem: (state, payload) => payload,
+    setCheckedComplited: (state, payload) => payload,
   },
   effects: {
-    add(){
+    add() {
       this.addItem();
+    },
+    remove(payload, rootState) {
+      const {
+        items,
+      } = rootState;
+      const newItems = items.filter(_item => _item.id !== payload );
+      this.removeItem(newItems);
+    },
+    checkComplited(payload, rootState){
+      const {
+        items,
+      } = rootState;
+
+      const newItems = items.map(_item => {
+        if (_item.id === payload) {
+          return {
+            ..._item,
+            completed: _item.completed ? false : Date.now(),
+          }
+        }
+
+        return _item;
+      });
+      this.setCheckedComplited(newItems);
+      // setLocalStorage();
     },
   }
 };
